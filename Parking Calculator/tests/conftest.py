50e07e52bb559d0603b7fdf966f4c5ecfa609c37
Michał Święciło <michal_00-00@o2.pl>
2025-01-14 13:58:19 +0100

    Fixture for automatic driver creation added

---
+++ Parking Calculator/tests/conftest.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import pytest
+
+
+# @pytest.fixture(params=["firefox", "edge"])
+@pytest.fixture(params=["edge"])
+def driver(request):
+    # browser = request.config.getoption("--browser")
+    browser = request.param
+    print(f"Creating {browser} driver")
+
+    if browser == "firefox":
+        my_driver = webdriver.Firefox()
+    elif browser == "edge":
+        my_driver = webdriver.Edge()
+    else:
+        raise TypeError(f"Expected 'firefox' or 'edge' but got {browser}")
+
+    my_driver.implicitly_wait(1)
+
+    yield my_driver
+
+    print(f"\nClosing {browser} driver")
+    # my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="firefox", help="browser to execute tests (chrome, firefox)"
+    )
