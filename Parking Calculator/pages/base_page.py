2613fe50a65dad88fdb2111c4235af82700a28e7
Michał Święciło <michal_00-00@o2.pl>
2025-01-14 11:20:56 +0100

    skleton classes added

--- Parking Calculator/pages/base_page.py
+++ Parking Calculator/pages/base_page.py
@@ -1,4 +1,6 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class BasePage:
@@ -6,3 +8,24 @@ class BasePage:
 
     def __init__(self, driver: WebDriver):
         self._driver = driver
+
+    def _open_url(self, url: str):
+        self._driver.get(url)
+
+    def _is_displayed(self, locator: tuple) -> bool:
+        try:
+            return self._find(locator).is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def _find(self, locator: tuple) -> WebElement:
+        return self._driver.find_element(*locator)
+
+    def _type(self, locator: tuple, text: str):
+        self._find(locator).send_keys(text)
+
+    def _click(self, locator: tuple):
+        self._find(locator).click()
+
+    def _get_text(self, locator: tuple) -> str:
+        return self._find(locator).text
--- Parking Calculator/pages/cost_calculator_page.py
+++ Parking Calculator/pages/cost_calculator_page.py
@@ -6,4 +6,19 @@ from pages.base_page import BasePage
 class CostCalculatorPage(BasePage):
     """Cost calculator page - The main and only page of the app."""
 
-    pass
+    __url = "https://www.shino.de/parkcalc/"
+
+    def __init__(self, driver: WebDriver):
+        super().__init__(driver)
+
+    def open(self):
+        super()._open_url(self.__url)
+
+    def calculate_cost(self, parking_type: str, entry_date: str, entry_time: str, leaving_date: str, leaving_time: str):
+        pass
+
+    def get_cost(self) -> float:
+        pass
+
+    def get_parking_time(self) -> int:
+        pass
