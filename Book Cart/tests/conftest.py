c998683a6d499c81b47d2247134b812d5ecc3d00
Michał Święciło <michal_00-00@o2.pl>
2025-01-29 23:40:10 +0100

    Simple login page test created

--- Book Cart/tests/conftest.py
+++ Book Cart/tests/conftest.py
@@ -1,2 +1,32 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver.")
+
+    if browser == "firefox":
+        my_driver = webdriver.Firefox()
+    elif browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "safari":
+        my_driver = webdriver.Safari()
+    elif browser == "edge":
+        my_driver = webdriver.Edge()
+    else:
+        raise TypeError(f"Expected 'firefox', 'chrome', 'safari' or 'edge' but got {browser}")
+
+    my_driver.maximize_window()
+
+    yield my_driver
+
+    print(f"\nClosing {browser} driver.")
+    # my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="firefox", help="browser to execute tests (chrome, firefox)"
+    )
--- Book Cart/tests/temp_test.py
+++ Book Cart/tests/temp_test.py
@@ -1,2 +1,18 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from pages.login_page import LoginPage
+
+
+class TestAccountAccess:
+    """
+    Test class containing test methods for log-in and registration scenarios.
+    """
+
+    def test_login(self, driver: WebDriver):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.log_in("MichalS", "StrongPassword123")
+
+
+
+
