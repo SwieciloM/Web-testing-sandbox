4da47e74d2dbfaa8758bf8372ef078f6989f7621
Michał Święciło <michal_00-00@o2.pl>
2025-02-02 22:20:41 +0100

    Simple register page test created (NOT WORKING)

--- Book Cart/pages/register_page.py
+++ Book Cart/pages/register_page.py
@@ -2,20 +2,21 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
+from enums.gender import Gender
 
 
 class RegisterPage(BasePage):
     """Register page for new accounts creation."""
 
     __url = "https://bookcart.azurewebsites.net/register"
-    __first_name_field_locator = ()
-    __last_name_field_locator = ()
-    __username_field_locator = ()
-    __password_field_locator = ()
-    __password_confirm_field_locator = ()
-    __gender_male_checkbox_locator = ()
-    __gender_female_checkbox_locator = ()
-    __register_button_locator = ()
+    __first_name_field_locator = (By.ID, "mat-input-4")
+    __last_name_field_locator = (By.ID, "mat-input-5")
+    __username_field_locator = (By.ID, "mat-input-6")
+    __password_field_locator = (By.ID, "mat-input-7")
+    __password_confirm_field_locator = (By.ID, "mat-input-8")
+    __gender_male_checkbox_locator = (By.ID, "mat-radio-0-input")
+    __gender_female_checkbox_locator = (By.ID, "mat-radio-1-input")
+    __register_button_locator = (By.XPATH, "//button[.//span[text()='Register']]")
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
@@ -24,17 +25,26 @@ class RegisterPage(BasePage):
         """
         Opens the register page in the browser.
         """
-
         super()._open_url(self.__url)
 
-    def register(self, first_name: str, last_name: str, gender: str, username: str, password: str):
+    def register(self, first_name: str, last_name: str, gender: Gender, username: str, password: str):
         """
+        Fills the register form and submits it.
 
+        :param first_name: User's first name
+        :param last_name: User's last name
+        :param gender: User's gender
+        :param username: Username for new account
+        :param password: Password for new account
         """
         super()._type(self.__first_name_field_locator, first_name)
         super()._type(self.__last_name_field_locator, last_name)
         super()._type(self.__username_field_locator, username)
         super()._type(self.__password_field_locator, password)
-        super()._type(self.__gender_female_checkbox_locator, gender)
+        super()._type(self.__password_confirm_field_locator, password)
+        if gender == Gender.MALE:
+            super()._click(self.__gender_male_checkbox_locator)
+        elif gender == Gender.FEMALE:
+            super()._click(self.__gender_female_checkbox_locator)
         super()._click(self.__register_button_locator)
 
--- Book Cart/tests/temp_test.py
+++ Book Cart/tests/temp_test.py
@@ -1,17 +1,24 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 
+from enums.gender import Gender
 from pages.login_page import LoginPage
+from pages.register_page import RegisterPage
 
 
-class TestAccountAccess:
+class TestAccountFunctionality:
     """
     Test class containing test methods for log-in and registration scenarios.
     """
 
-    def test_login(self, driver: WebDriver):
-        login_page = LoginPage(driver)
-        login_page.open()
-        login_page.log_in("MichalS", "StrongPassword123")
+    # def test_login(self, driver: WebDriver):
+    #     login_page = LoginPage(driver)
+    #     login_page.open()
+    #     login_page.log_in("MichalS", "StrongPassword123")
+
+    def test_register(self, driver: WebDriver):
+        register_page = RegisterPage(driver)
+        register_page.open()
+        register_page.register("Daniel", "Sokolik", Gender.MALE, "DanielS", "DanielS1$")
 
 
 
